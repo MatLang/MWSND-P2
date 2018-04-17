6c980e84dbc66706f484ffc56a1675b2e8c3a0c8
mala <mala@censhare.com>
2018-04-17 16:55:37 +0200

    add scripts:main task to gulpfile

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -4,3 +4,10 @@ var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+
+gulp.tasks('scripts:main', function () {
+    browserify(['js/main.js', 'js/dbhelper.js', 'js/app.js'])
+        .transform("babelify", { presets: ["env", "react"] })
+        .bundle()
+        .pipe(gulp.dest("./bundle.js"));
+})
